148feab76364ec1ddccd6106c077002c1b87a70c
Adams Shittu <shttdms@gmail.com>
2018-12-17 11:09:31 +0100

    adjust the port

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ if (app.get("env") === "development") {
     });
 }
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port 3000!");
 });
