9404f12f0ac35ce8264028a288627748406a0677
Adams Shittu <shttdms@gmail.com>
2018-12-17 11:05:38 +0100

    add production connectionString

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-let connectionString = "postgres://postgres@localhost:5432/fishes-app"
+let connectionString = process.env.DATABASE_URL || "postgres://postgres@localhost:5432/fishes-app"
 const client = new Client(connectionString);
 
 client.connect();
